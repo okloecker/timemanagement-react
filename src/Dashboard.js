36bd7a88957be7a3ebe65bcfef590215245c3008
Olaf Klöcker <npmokl@gmx.com>
2020-05-06 17:35:15 +0100

    feat: [RecordsTable] display records between start and end dates defined by Date Pickers from Dashboard; use react-query for background fetching/caching.

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,17 +1,18 @@
 import React from "react";
-// import PropTypes from 'prop-types';
+import { Formik } from "formik";
 import { Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import DateFnsUtils from "@date-io/date-fns";
+import isDate from "date-fns/isDate";
 import startOfMonth from "date-fns/startOfMonth";
 import endOfMonth from "date-fns/endOfMonth";
+import isBefore from "date-fns/isBefore";
+import isEqual from "date-fns/isEqual";
 import {
   MuiPickersUtilsProvider,
-  KeyboardTimePicker,
   KeyboardDatePicker
 } from "@material-ui/pickers";
-
-// timerecords?dateFrom=2016-07-28&dateTo=2016-07-31
+import RecordsTable from "RecordsTable";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -29,78 +30,82 @@ const useStyles = makeStyles(theme => ({
 const Dashboard = props => {
   const classes = useStyles();
 
-  const [selectedStartDate, setSelectedStartDate] = React.useState(
-    startOfMonth(new Date())
-  );
-  const [selectedEndDate, setSelectedEndDate] = React.useState(
-    endOfMonth(new Date())
-  );
-
   return (
     <Grid container className={classes.root} justify="center" spacing={2}>
       <Grid item xs={10}>
         <Paper className={classes.control} elevation={2}>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container justify="center" spacing={5}>
-              <Grid item>
-                <KeyboardDatePicker
-                  margin="normal"
-                  id="date-picker-dialog-start"
-                  label="Start Date"
-                  format="MM/dd/yyyy"
-                  value={selectedStartDate}
-                  onChange={setSelectedStartDate}
-                  KeyboardButtonProps={{
-                    "aria-label": "change start date"
-                  }}
+          <Formik
+            initialValues={{
+              selectedStartDate: startOfMonth(new Date()),
+              selectedEndDate: endOfMonth(new Date())
+            }}
+            validate={values => {
+              const errors = {};
+              if (
+                isDate(values.selectedStartDate) &&
+                isDate(values.selectedEndDate) &&
+                !isBefore(values.selectedStartDate, values.selectedEndDate) &&
+                !isEqual(values.selectedStartDate, values.selectedEndDate)
+              ) {
+                errors.selectedEndDate =
+                  "End date must be at or after start date.";
+              }
+              return errors;
+            }}
+          >
+            {({ values, errors, setFieldValue }) => (
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justify="center" spacing={5}>
+                  <Grid item>
+                    <KeyboardDatePicker
+                      margin="normal"
+                      id="date-picker-dialog-start"
+                      label="Start Date"
+                      format="MM/dd/yyyy"
+                      value={values.selectedStartDate}
+                      onChange={e =>
+                        isDate(e) &&
+                        e.toString() !== "Invalid Date" &&
+                        setFieldValue("selectedStartDate", e)
+                      }
+                      error={!!errors.selectedStartDate}
+                      helperText={errors.selectedStartDate}
+                      KeyboardButtonProps={{
+                        "aria-label": "change start date"
+                      }}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <KeyboardDatePicker
+                      margin="normal"
+                      id="date-picker-dialog-end"
+                      label="End Date"
+                      format="MM/dd/yyyy"
+                      value={values.selectedEndDate}
+                      onChange={e =>
+                        isDate(e) &&
+                        e.toString() !== "Invalid Date" &&
+                        setFieldValue("selectedEndDate", e)
+                      }
+                      error={!!errors.selectedEndDate}
+                      helperText={errors.selectedEndDate}
+                      KeyboardButtonProps={{
+                        "aria-label": "change end date"
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+                <RecordsTable
+                  startDate={values.selectedStartDate}
+                  endDate={values.selectedEndDate}
                 />
-              </Grid>
-              <Grid item>
-                <KeyboardTimePicker
-                  margin="normal"
-                  id="time-picker-start"
-                  label="Start Time"
-                  value={selectedStartDate}
-                  onChange={setSelectedStartDate}
-                  KeyboardButtonProps={{
-                    "aria-label": "change start time"
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <KeyboardDatePicker
-                  margin="normal"
-                  id="date-picker-dialog-end"
-                  label="End Date"
-                  format="MM/dd/yyyy"
-                  value={selectedEndDate}
-                  onChange={setSelectedEndDate}
-                  KeyboardButtonProps={{
-                    "aria-label": "change end date"
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <KeyboardTimePicker
-                  margin="normal"
-                  id="time-picker-end"
-                  label="End Time"
-                  value={selectedEndDate}
-                  onChange={setSelectedEndDate}
-                  KeyboardButtonProps={{
-                    "aria-label": "change end time"
-                  }}
-                />
-              </Grid>
-            </Grid>
-          </MuiPickersUtilsProvider>
+              </MuiPickersUtilsProvider>
+            )}
+          </Formik>
         </Paper>
       </Grid>
     </Grid>
   );
 };
-
-// Dashboard.propTypes = {
-// };
 
 export default Dashboard;
---
+++ src/RecordsTable.js
@@ -0,0 +1,144 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  LinearProgress,
+  Snackbar,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useQuery } from "react-query";
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
+import isDate from "date-fns/isDate";
+import { getCookie } from "helpers/cookies";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650
+  },
+  root: {
+    minWidth: 275
+  },
+  title: {
+    fontSize: 20,
+    textAlign: "center"
+  }
+});
+
+const getJson = async data => {
+  if (
+    (((data || {}).headers || {}).get("content-type") || "").includes(
+      "application/json"
+    )
+  ) {
+    return await data.json();
+  } else return "NO_JSON";
+};
+
+const fetchRecords = async (key, { startDate, endDate, authToken }) => {
+  try {
+    const from = format(
+      isDate(startDate) ? startDate : parseISO(startDate),
+      "yyyy-MM-dd"
+    );
+    const to = format(
+      isDate(endDate) ? endDate : parseISO(endDate),
+      "yyyy-MM-dd"
+    );
+    const result = await fetch(
+      `/app/timerecords?dateFrom=${from}&dateTo=${to}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-AUTH-TOKEN": authToken
+        }
+      }
+    );
+    return getJson(result);
+  } catch (err) {
+    console.log("ERROR:", err);
+    debugger;
+    return err;
+  }
+};
+
+const RecordsTable = props => {
+  const classes = useStyles();
+
+  const authToken = getCookie("authToken");
+
+  const { status, data, error } = useQuery(
+    [
+      "records",
+      { startDate: props.startDate, endDate: props.endDate, authToken }
+    ],
+    fetchRecords
+  );
+
+  if (status === "loading") {
+    return <LinearProgress />;
+  }
+
+  if (status === "error") {
+    return (
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left"
+        }}
+        open={status === "error"}
+        autoHideDuration={6000}
+        message={error}
+      />
+    );
+  }
+
+  if (!data || !data.length) {
+    return (
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            There is no data to display for these dates, try to change 
+            start and end dates.
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  }
+
+  return (
+    <TableContainer>
+      <Table className={classes.table} aria-label="time records">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell align="right">Hours</TableCell>
+            <TableCell>Note</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(row => (
+            <TableRow key={row.id} row={row}>
+              <TableCell>{row.date}</TableCell>
+              <TableCell align="right">{row.timeString}</TableCell>
+              <TableCell>{row.note}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default RecordsTable;
