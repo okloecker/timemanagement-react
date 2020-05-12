ce000a3be90d7ddeb38b3a756d4810e869a10f8a
Olaf Klöcker <npmokl@gmx.com>
2020-05-12 18:07:38 +0100

    fix: [App] if logout fails (e.g. with 401 unauthorized), delete cookies and localStorage anyway

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import axios from "axios";
 
 /*
  * Entry point for app, containing the routes and logout handling.
-*/
+ */
 function App() {
   const [authToken, setAuthToken] = React.useState();
   const [logoutResult, setLogoutResult] = React.useState({});
@@ -36,23 +36,27 @@ function App() {
   const [mutate] = useMutation(getLogout);
   const handleLogout = async () => {
     try {
-      await mutate(null,
-              {
-                onSuccess: response => {
-                  setLogoutResult({ ok: true, message: response.data.success.message });
-                  deleteCookie("authToken");
-                  setAuthToken(null);
-                  storage.local.removeItem("userInfo");
-                },
-                onError: error => {
-                  if (error.response.data) {
-                    if (error.response.data.error) {
-                      setLogoutResult({ ok: false, message: error.response.data.error.message });
-                    }
-                  }
-                }
-              }
-      );
+      await mutate(null, {
+        onSuccess: response => {
+          setLogoutResult({ ok: true, message: response.data.success.message });
+          deleteCookie("authToken");
+          setAuthToken(null);
+          storage.local.removeItem("userInfo");
+        },
+        onError: error => {
+          if (error.response.data) {
+            if (error.response.data.error) {
+              setLogoutResult({
+                ok: false,
+                message: error.response.data.error.message
+              });
+            }
+          }
+          deleteCookie("authToken");
+          setAuthToken(null);
+          storage.local.removeItem("userInfo");
+        }
+      });
     } catch (err) {
       console.log("POST logout error", err);
     }
