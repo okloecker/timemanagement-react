d34278ff5fe6a2429e683515c75fffa37250da07
Olaf Klöcker <npmokl@gmx.com>
2020-05-31 21:46:13 +0100

    feat: [ReadonlyRecord] group records by date and move day information to Divider

--- src/records/ReadonlyRecord.js
+++ src/records/ReadonlyRecord.js
@@ -1,15 +1,28 @@
-import React from "react";
-import { Box, Divider, Grid } from "@material-ui/core";
+import { Box, Divider, Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import format from "date-fns/format";
 import isValid from "date-fns/isValid";
 import { minToArr } from "helpers/time";
-import { StartEditControls } from "./EditControls";
+import React from "react";
 import TimeDuration from "TimeDuration";
 
-const useStyles = makeStyles({
-  duration: { fontStyle: "italic", textAlign: "right" }
-});
+import { StartEditControls } from "./EditControls";
+
+const useStyles = makeStyles(theme => ({
+  duration: {
+    textAlign: "right",
+    timeDuration: { color: "#3f51b5" },
+    fontWeight: 200,
+    color: "#3f51b5"
+  },
+  dividerFullWidth: {
+    margin: `5px 0 0 ${theme.spacing(2)}px`,
+
+    textAlign: "center"
+  },
+  timeHH: {},
+  timeMM: { fontSize: "smaller" }
+}));
 
 const ReadonlyRecord = ({
   id,
@@ -26,43 +39,51 @@ const ReadonlyRecord = ({
   return (
     <Grid container spacing={1}>
       <Grid item xs={12}>
-        {newDay && <Divider variant="fullWidth" />}
+        {newDay && (
+          <>
+            <Divider />
+            <Typography
+              className={classes.dividerFullWidth}
+              color="textSecondary"
+              display="block"
+              variant="caption"
+            >
+              {format(startTime, "dd LLL yyyy")}
+            </Typography>
+          </>
+        )}
       </Grid>
 
       {/* Edit/Cancel etc icons */}
-      <Grid item xs={12} sm={1}>
+      <Grid item xs={4} sm={4} md={1}>
         <StartEditControls setEditing={setEditing} id={id} setStop={setStop} />
       </Grid>
 
-      {/* StartTime field */}
-      <Grid item xs={12} sm={5} md={2} lg={2} xl={2}>
+      <Grid item xs={4} sm={4} md={2} lg={2} xl={3}>
+        {/* StartTime field */}
         <Box m={1}>
-          <div>{format(startTime, dateTimeFormat)}</div>
-        </Box>
-      </Grid>
-
-      {/* EndTime field */}
-      <Grid item xs={12} sm={4} md={1} lg={1} xl={2}>
-        <Box m={1}>
-          <div>
-            {endTime
-              ? isValid(endTime)
-                ? format(endTime, "HH:mm")
-                : "—"
-              : null}
-          </div>
+          <FormatHHMM d={startTime} classes={classes} />
+          &ndash;
+          {/* EndTime field */}
+          {endTime ? (
+            isValid(endTime) ? (
+              <FormatHHMM d={endTime} classes={classes} />
+            ) : (
+              "—"
+            )
+          ) : null}
         </Box>
       </Grid>
 
       {/* duration field */}
-      <Grid item xs={12} sm={2} md={1}>
+      <Grid item xs={4} sm={4} md={1}>
         <Box m={1} className={classes.duration}>
-          <TimeDuration {...minToArr(durationMinutes)} fallback="—"/>
+          <TimeDuration {...minToArr(durationMinutes)} fallback="—" />
         </Box>
       </Grid>
 
       {/* Note field */}
-      <Grid item xs={12} sm={12} md={7} lg={7} xl={6}>
+      <Grid item xs={12} sm={12} md={8} lg={8} xl={6}>
         <Box m={1}>
           {(note || "").split("\n").map((line, i, arr) => (
             <div style={{ overflow: "auto" }} key={i + line}>
@@ -74,5 +95,12 @@ const ReadonlyRecord = ({
     </Grid>
   );
 };
+
+const FormatHHMM = ({d, classes}) => (
+  <>
+    <span className={classes.timeHH}>{format(d, "HH")}</span>:
+    <span className={classes.timeMM}>{format(d, "mm")}</span>
+  </>
+);
 
 export default React.memo(ReadonlyRecord);
